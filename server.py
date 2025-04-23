6c292fddd9031370dd86855b482d2bf6b14f0bab
EvelynnnnnnZ <161157176+EvelynnnnnnZ@users.noreply.github.com>
2025-04-23 01:35:44 -0400

    Update server.py

--- server.py
+++ server.py
@@ -15,5 +15,27 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
+# page 2-5, learning material
+@app.route('/start')
+def learn_start():
+    return render_template('learn_2.html')
+
+@app.route('/learn/2')
+def learn_2():
+    return render_template('learn_2.html')
+
+@app.route('/learn/3')
+def learn_3():
+    return render_template('learn_3.html')
+
+@app.route('/learn/4')
+def learn_4():
+    return render_template('learn_4.html')
+
+@app.route('/learn/5')
+def learn_5():
+    return render_template('learn_5.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
